04da7b554fd8b2a82537157db47d657ef44e16a7
Adominguezes <alberto@adominguez.es>
2017-07-09 12:18:28 +0200

    fix(scss, js, html) add 4 columns to components

--- webcomponents-page.js
+++ webcomponents-page.js
@@ -43,11 +43,6 @@
         type: String
       }
     },
-    attached: function() {
-      this.components = this.categories[0].webcomponents;
-      this.categoryColor = this.categories[0].categoryColor;
-      this.selected = this.categories[0].categoryName;
-    },
     _setWebcomponents: function(e) {
       var model = e.model.item;
       this.$.components.hidden = false;
